db9fb8380869c5da91ad8435e59039823163ada3
Jason <jasondelossantos1@outlook.com>
2025-05-08 11:56:54 -0400

    Static andd dynamic rendering

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import styles from '@/app/ui/home.module.css';    //black triangle
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className={styles.shape}               /*black triangle *//>         
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
